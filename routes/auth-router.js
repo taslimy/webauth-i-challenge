b8b97f6c7b5bd27650570b9177fb137638b45a37
taslimy <taslimer@gmail.com>
2019-06-11 14:31:50 -0400

    Added session and cookies

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -6,7 +6,9 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
+  // When im destructuring don't use CONST when registering
   let { username, password } = req.body;
+
   if (!username && !password) {
     res
       .status(401)
@@ -30,10 +32,11 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  let { username, password } = req.body;
+  const { username, password } = req.body;
   try {
     const user = await Users.findBy({ username }).first();
     if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.username = user.username;
       res.status(200).json({ message: `Welcome ${user.username}!` });
     } else {
       res.status(401).json({ message: "Invalid Credentials" });
@@ -41,6 +44,19 @@ router.post("/login", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
+  }
+});
+
+// Logout
+router.get("/logout", async (req, res) => {
+  try {
+    if (req.session) {
+      req.session.destroy();
+    }
+    res.status(200).json({ message: "you have logged out bye" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "unable to logout" });
   }
 });
 
--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 
 const server = express();
+
+const sessionConfig = {
+  name: "monkey", // by default it would be sid
+  secret: "keep it secret, keep it safe",
+  resave: false, // if there are no changes to the session don't save it again.
+  saveUninitialized: true, // for GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10, // milliseconds
+    secure: false, // send cookie only over https, set to true in production
+    httpOnly: true // always set to true, it means client JS can't access the cookie
+  },
+  store: new knexSessionStore({
+    knex: require("./data/dbConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
 server.use(express());
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+server.use(session(sessionConfig));
 server.use(logger);
 
 server.get("/", (req, res) => {
   res.send(`<h2> TESTING IF SERVER IS LIVE! </h2>`);
 });
-
-// Middlewares
 
 // Define Routes
 server.use("/api/auth", require("./routes/auth-router")); // register & login
